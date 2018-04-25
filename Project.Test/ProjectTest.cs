7a65d870c8db19d027f9765641e0756aede45622
Michael L. Stephens <mlstephens@.zrtlab.com>
2018-04-25 13:31:23 -0700

    Refactored the argument name values to match lesson examples based on PR comments.

--- Project.Test/ProjectTest.cs
+++ Project.Test/ProjectTest.cs
@@ -13,11 +13,13 @@ namespace Project.Test
         public void CommandLineArguments_ValidArgumentFormat()
         {
             //arrange
-            string[] arguments = new string[2];
+            string[] arguments = new string[4];
             double expectedTotal = 26.61;
 
-            arguments[0] = "added=1.5,2,3.5,4,66.60";
-            arguments[1] = "subtracted=1,2,5.9,42.09";
+            arguments[0] = "-added";
+            arguments[1] = "1.5,2,3.5,4,66.60";
+            arguments[2] = "-subtracted";
+            arguments[3] = "1,2,5.9,42.09";
 
             //act
             ArgumentsClass argumentClass = new ArgumentsClass(arguments);
@@ -31,10 +33,12 @@ namespace Project.Test
         public void CommandLineArguments_InvalidArgumentFormat()
         {
             //arrange
-            string[] arguments = new string[2];
+            string[] arguments = new string[4];
 
-            arguments[0] = "added=1,2,3,4,5";
-            arguments[1] = "subtracted:6,7,8,9,10";
+            arguments[0] = "added";
+            arguments[1] = "1,2,3,4,5";
+            arguments[2] = "subtracted";
+            arguments[3] = "6,7,8,9,10";
 
             //act
             ArgumentsClass argumentClass = new ArgumentsClass(arguments);
@@ -60,11 +64,13 @@ namespace Project.Test
         public void CommandLineArguments_NonNumericArgumentValues()
         {
             //arrange
-            string[] arguments = new string[2];
+            string[] arguments = new string[4];
             double expectedTotal = 3.25;
 
-            arguments[0] = "added=1,2,3.5,b,c,d";
-            arguments[1] = "subtracted=1.25,2.0,e,f,g,i";
+            arguments[0] = "-added";
+            arguments[1] = "1,2,3.5,b,c,d";
+            arguments[2] = "-subtracted";
+            arguments[3] = "1.25,2.0,e,f,g,i";
 
             //act
             ArgumentsClass argumentClass = new ArgumentsClass(arguments);
@@ -78,10 +84,11 @@ namespace Project.Test
         public void CommandLineArguments_SingleArgument_Added()
         {
             //arrange
-            string[] arguments = new string[1];
+            string[] arguments = new string[2];
             double expectedTotal = 6.50;
 
-            arguments[0] = "added=1,2,3.5";
+            arguments[0] = "-added";
+            arguments[1] = "1,2,3.5";
 
             //act
             ArgumentsClass argumentClass = new ArgumentsClass(arguments);
@@ -95,10 +102,11 @@ namespace Project.Test
         public void CommandLineArguments_SingleArgument_Subtracted()
         {
             //arrange
-            string[] arguments = new string[1];
+            string[] arguments = new string[2];
             double expectedTotal = -6.50;
 
-            arguments[0] = "subtracted=1,2,3.5";
+            arguments[0] = "-subtracted";
+            arguments[1] = "1,2,3.5";
 
             //act
             ArgumentsClass argumentClass = new ArgumentsClass(arguments);
