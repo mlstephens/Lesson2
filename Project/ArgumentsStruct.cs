0752f0ff67e025be20b062682232f29dbe47eaae
Michael L. Stephens <mlstephens@.zrtlab.com>
2018-04-25 15:46:51 -0700

    Minor change to GetNumbers method, moved .Find to newline.

--- Project/ArgumentsStruct.cs
+++ Project/ArgumentsStruct.cs
@@ -34,7 +34,8 @@ namespace Lesson2
             //find that namevalue
             argumentKeyValues = _argumentsList.SkipWhile(a => a != argumentNameValue)
                 .Skip(1)
-                .DefaultIfEmpty(string.Empty).First()
+                .DefaultIfEmpty(string.Empty)
+                .First()
                 .Split(_valueSeperator)
                 .Where(a => Double.TryParse(a, out double value))
                 .Select(Double.Parse)
