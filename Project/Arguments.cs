b23e03d1b1e0624b2d199628919d35436ef368c1
Michael L. Stephens <mlstephens@.zrtlab.com>
2018-04-27 14:40:36 -0700

    Modified IsValidCommanLine, GetNumbers and constructor code.

--- Project/Arguments.cs
+++ Project/Arguments.cs
@@ -14,11 +14,10 @@ namespace Lesson2
 
         public Arguments(string[] arguments)
         {
-            _arguments = arguments.Select(a => a.ToLower()).ToArray();
+            _arguments = arguments;
         }
 
-        //public bool IsCommandLineValid { get => (_arguments.Length >= 1 && _arguments.Length <= 4) && (_arguments.Any(a => a == _addNameValue) || _arguments.Any(a => a == _subtractNameValue)) ; }
-        public bool IsCommandLineValid { get => _arguments.Any(a => a == _addNameValue) || _arguments.Any(a => a == _subtractNameValue); }
+        public bool IsCommandLineValid { get => _arguments.Any(a => String.Compare(a, _addNameValue, true) == 0) || _arguments.Any(a => String.Compare(a, _subtractNameValue, true) == 0); }
 
         private double[] NumbersToAdd { get => GetNumbers(_addNameValue); }
 
@@ -27,7 +26,7 @@ namespace Lesson2
         private double[] GetNumbers(string argumentNameValue)
         {
             //find that namevalue
-            double[] argumentKeyValues = _arguments.SkipWhile(a => a != argumentNameValue)
+            double[] argumentKeyValues = _arguments.SkipWhile(a => a.ToLower() != argumentNameValue.ToLower())
                 .Skip(1)
                 .DefaultIfEmpty(string.Empty)
                 .First()
