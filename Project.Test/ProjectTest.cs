5c0ccf9d321ba75e0fe47f657850a2ca97d0acf4
Michael L. Stephens <mlstephens@.zrtlab.com>
2018-04-27 15:40:33 -0700

    Refactored unittests to use same data as class unit test based on PR comments about DRY.

--- Project.Test/ProjectTest.cs
+++ Project.Test/ProjectTest.cs
@@ -12,8 +12,8 @@ namespace Project.Test
         public void Arguments_WithValidFormat()
         {
             //arrange
-            string[] argArray = new string[] { "-added", "25.983,15.09,9,33.7838", "-subtracted", "10,15.78383,2.09" };
-            double expectedTotal = 55.983;
+            string[] argArray = new string[] { "-added", "1.5,2,3.5,4,66.60", "-subtracted", "1,2,5.9,42.09" };
+            double expectedTotal = 26.61;
 
             //act
             Arguments arguments = new Arguments(argArray);
@@ -86,8 +86,8 @@ namespace Project.Test
         public void Arguments_WithNonNumericValues()
         {
             //arrange
-            string[] argArray = new string[] { "-added", "65.38,49.99,A,d", "-subtracted", "15.00003,22.93,Z" };
-            double expectedTotal = 77.44;
+            string[] argArray = new string[] { "-added", "1,2,3.5,b,c,d", "-subtracted", "1.25,2.0,e,f,g,i" };
+            double expectedTotal = 3.25;
 
             //act
             Arguments arguments = new Arguments(argArray);
@@ -102,8 +102,8 @@ namespace Project.Test
         public void Arguments_WithSingleArgument_Added()
         {
             //arrange
-            string[] argArray = new string[] { "-added", "103.9838,75.0393" };
-            double expectedTotal = 179.0231;
+            string[] argArray = new string[] { "-added", "1,2,3.5" };
+            double expectedTotal = 6.50;
 
             //act
             Arguments arguments = new Arguments(argArray);
@@ -118,8 +118,8 @@ namespace Project.Test
         public void Arguments_WithSingleArgument_Subtracted()
         {
             //arrange
-            string[] argArray = new string[] { "-subtracted", "364.993,1.009,-2.5,-34.0999" };
-            double expectedTotal = -329.4021;
+            string[] argArray = new string[] { "-subtracted", "1,2,3.5" };
+            double expectedTotal = -6.50;
 
             //act
             Arguments arguments = new Arguments(argArray);
